9d2d6ee19aa8e2cbc8366da13a45f7c5f214798f
Thomas Labrusse <labrusse.thomas@gmail.com>
2022-05-23 13:56:35 +0200

    favicon fix

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Head from 'next/head'
 
 import Header from './Header'
 import Footer from './Footer'
@@ -9,9 +8,6 @@ import classes from './Layout.module.css'
 const Layout = (props) => {
 	return (
 		<div className={classes.layout}>
-			<Head>
-				<link rel='shortcut icon' href='/favicon.ico' />
-			</Head>
 			<Header />
 			<main>{props.children}</main>
 			<Footer />
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import '../styles/styles.css'
 
 import Layout from '../components/layout/Layout'
@@ -5,9 +6,18 @@ import Layout from '../components/layout/Layout'
 function MyApp({ Component, pageProps }) {
 	// The Component prop is the active page
 	return (
+		<>
+			<Head>
+				<link
+					rel='shortcut icon'
+					type='image/x-icon'
+					href='/images/favicon.ico'
+				/>
+			</Head>
 			<Layout>
 				<Component {...pageProps} />
 			</Layout>
+		</>
 	)
 }
 
